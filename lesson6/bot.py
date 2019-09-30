98bc40eae241741db2a9f47a398c0e3142a12967
gonchigor <gonchigor@tut.by>
2019-10-01 00:35:25 +0300

    telegram bot

--- lesson6/bot.py
+++ lesson6/bot.py
@@ -1,7 +1,7 @@
 import requests
 import json
 from time import sleep
-from token import token
+from token_auth import token
 
 last_updates = 0
 URL = 'https://api.telegram.org/bot' + token + '/'
@@ -13,15 +13,16 @@ def get_updates():
     
 def get_message():
     data = get_updates()
-    chat_id = data['result'][-1]['message']['chat']['id']
-    global last_updates
-    cur_update = data['result'][-1]['update_id']
-    if last_updates != cur_update:
-        last_updates = cur_update
-        message_text = data['result'][-1]['message']['text']
-        message = {'chat_id': chat_id,
-                'text': message_text}
-        return message
+    if len(data['result']):
+        chat_id = data['result'][-1]['message']['chat']['id']
+        global last_updates
+        cur_update = data['result'][-1]['update_id']
+        if last_updates != cur_update:
+            last_updates = cur_update
+            message_text = data['result'][-1]['message']['text']
+            message = {'chat_id': chat_id,
+                    'text': message_text}
+            return message
     return None
 
 
@@ -29,17 +30,21 @@ def send_message(chat_id, text):
     url = URL + 'sendmessage?chat_id={}&text={}'.format(chat_id, text)
     requests.get(url)
 
+
 def main():
 #     d = get_updates()
 #     with open('updates.json', 'w') as file:
 #         json.dump(d, file, indent=2, ensure_ascii=False)
     # print(get_message())
-    answer = get_message()
-    chat_id = answer['chat_id']
-    if 'ничего' in answer['text']:
-        send_message(chat_id, 'Тогда проваливай')
-    else:
-        send_message(chat_id, 'Что тебе нужно?')
+    while True:
+        answer = get_message()
+        if answer is not None:
+            chat_id = answer['chat_id']
+            if 'ничего' in answer['text']:
+                send_message(chat_id, 'Тогда проваливай')
+            else:
+                send_message(chat_id, 'Что тебе нужно?')
+        sleep(3)
 
 if __name__ == '__main__':
     main()
