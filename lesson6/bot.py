450f48d924c42e53f181b751bedc053e81357c81
gonchigor <gonchigor@tut.by>
2019-10-06 11:17:20 +0300

    some refactoring

--- lesson6/bot.py
+++ lesson6/bot.py
@@ -57,53 +57,49 @@ def main():
     #     with open('updates.json', 'w') as file:
     #     json.dump(d, file, indent=2, ensure_ascii=False)
     # print(get_message())
-    try:
-        while True:
-            answer = get_message()
-            if answer is not None:
-                chat_id = answer['chat_id']
-                answer_text = answer['text']
-                answer_text_words = answer_text.split()
-                if 'ничего' in answer['text']:
-                    send_message(chat_id, 'Тогда проваливай')
-                elif len(answer_text_words):
-                    if answer_text_words[0].upper() in ('/USD', '/EUR', '/RUB'):
-                        try:
-                            if len(answer_text_words) == 1:
-                                send_message(chat_id,
-                                             'Курс {} {} на сегодня: {}'.format(
-                                                *rate(answer_text_words[0][1:])))
-                            else:
-                                date_value = date(int(answer_text_words[3]),
-                                                  int(answer_text_words[2]),
-                                                  int(answer_text_words[1]))
-                                send_message(chat_id,
-                                             'Курс {1} {2} на {0}: {3}'.format(
-                                                date_value.isoformat(),
-                                                *rate(answer_text_words[0][1:],
-                                                      date_value)
-                                             ))
-                        except (KeyError, ValueError):
-                            send_message(chat_id, 'Something wrong!!!')
-                        except Exception as e:
-                            print(type(e))
-                            send_message(chat_id, "Incorrect command")
-                    elif answer_text_words[0].upper() == '/HABR':
+    while True:
+        answer = get_message()
+        if answer is not None:
+            chat_id = answer['chat_id']
+            answer_text = answer['text']
+            answer_text_words = answer_text.split()
+            if 'ничего' in answer['text']:
+                send_message(chat_id, 'Тогда проваливай')
+            elif len(answer_text_words):
+                if answer_text_words[0].upper() in ('/USD', '/EUR', '/RUB'):
+                    try:
                         if len(answer_text_words) == 1:
-                                messages = get_habr()
-                                for mes in messages:
-                                    send_message(chat_id, mes)
+                            send_message(chat_id,
+                                        'Курс {} {} на сегодня: {}'.format(
+                                        *rate(answer_text_words[0][1:])))
                         else:
-                            send_message(chat_id, get_habr(
-                                int(answer_text_words[1])
-                            ))
+                            date_value = date(int(answer_text_words[3]),
+                                              int(answer_text_words[2]),
+                                              int(answer_text_words[1]))
+                            send_message(chat_id,
+                                            'Курс {1} {2} на {0}: {3}'.format(
+                                            date_value.isoformat(),
+                                            *rate(answer_text_words[0][1:],
+                                                    date_value)
+                                            ))
+                    except (KeyError, ValueError):
+                        send_message(chat_id, 'Something wrong!!!')
+                    except Exception as e:
+                        send_message(chat_id, "Incorrect command")
+                elif answer_text_words[0].upper() == '/HABR':
+                    if len(answer_text_words) == 1:
+                            messages = get_habr()
+                            for mes in messages:
+                                send_message(chat_id, mes)
                     else:
-                        send_message(chat_id, 'Что тебе нужно?')
+                        send_message(chat_id, get_habr(
+                            int(answer_text_words[1])
+                        ))
                 else:
-                    send_message(chat_id, 'Not understand')
-            sleep(3)
-    except KeyError:
-        print("Invalid token")
+                    send_message(chat_id, 'Что тебе нужно?')
+            else:
+                send_message(chat_id, 'Not understand')
+        sleep(3)
 
 if __name__ == '__main__':
     main()
